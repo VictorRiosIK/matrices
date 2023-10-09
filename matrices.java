d6f646b88eb4565c776d166cae4eb170e372b586
Victor <vrios718@gmail.com>
2023-10-09 10:48:01 -0600

    modified

--- matrices.java
+++ matrices.java
@@ -1,9 +1,8 @@
 import java.io.*;
 import java.net.*;
 public class matrices{
-    // Crear una matriz de 6 renglones por 5 columnas (6x5)
-            static int N = 6; // Número de renglones
-            static int M = 5; // Número de columnas
+            static int N = 3000; // Número de renglones
+            static int M = 2000; // Número de columnas
             static int[][] matrizResultadoC=new int[N][N];
     public static void main(String[] args) {
         if (args.length != 1) {
@@ -16,12 +15,11 @@ public class matrices{
 
         switch (opcion) {
             case 1:
-                String serverAddress = "localhost"; // Dirección IP del servidor
-                int serverPort = 50001; // Puerto del servidor
+               
                 int numThreads = 2; // Número de hilos de cliente
                 Thread[] threads = new Thread[numThreads];
-                 int N = 6; // Número de renglones de la matriz
-                int M = 5; // Número de columnas de la matriz
+                 int N = 3000; // Número de renglones de la matriz
+                int M = 2000; // Número de columnas de la matriz
 
                 int[][] matrixA = new int[N][M];
                 int[][] matrixB = new int[M][N];
@@ -46,27 +44,27 @@ public class matrices{
                     }
                 }
 
-                System.out.println("Matriz A enviada al servidor");
+                /*  System.out.println("Matriz A enviada al servidor");
                 for (int i = 0; i < matrixA.length; i++) {
                     for (int j = 0; j < matrixA[i].length; j++) {
                         System.out.print(matrixA[i][j]+" ");
                     }
                     System.out.println();
-                }
+                }*/
                 
-                System.out.println("Matriz B transpuesta enviada al servidor:");
+                /*System.out.println("Matriz B transpuesta enviada al servidor:");
                 for (int i = 0; i < transposedMatrixB.length; i++) {
                     for (int j = 0; j < transposedMatrixB[i].length; j++) {
                         System.out.print(transposedMatrixB[i][j] + " ");
                     }
                     System.out.println();
-                }
+                }*/
                 // Definir el número de submatrices
                 int numSubmatrices = 6;
 
                 // Calcular el número de renglones por submatriz
                 int renglonesPorSubmatriz = N / numSubmatrices;
-
+                System.out.println("Renglones por sumbatriz:" + renglonesPorSubmatriz);
                 
                 // Crear arreglos de submatrices para los dos grupos
                 int[][][] submatricesA_grupo1 = new int[numSubmatrices / 2][renglonesPorSubmatriz][M];
@@ -89,7 +87,7 @@ public class matrices{
                         }
                     }
                 }
-                System.out.println("IMPRIMIENDO PRIMER GRUPO");
+                /*System.out.println("IMPRIMIENDO PRIMER GRUPO");
                 for (int i = 0; i < submatricesA_grupo1.length; i++) {
                     System.out.println("Grupo de submatriz " + i + ":");
                     int[][] submatriz = submatricesA_grupo1[i];
@@ -99,12 +97,12 @@ public class matrices{
                         }
                         System.out.println();
                     }
-                }
+                }*/
 
                 
                 try{ 
-                   threads[0] = new MatrixClient(serverAddress, serverPort, submatricesA_grupo1,transposedMatrixB,2);
-                   threads[1] = new MatrixClient(serverAddress, 50002, submatricesA_grupo2,transposedMatrixB,3);
+                   threads[0] = new MatrixClient("20.84.41.105", 50001, submatricesA_grupo1,transposedMatrixB,2);
+                   threads[1] = new MatrixClient("20.84.42.77", 50001, submatricesA_grupo2,transposedMatrixB,3);
                     
                 
                     for (int i = 0; i < numThreads; i++) {
@@ -140,7 +138,7 @@ public class matrices{
                     }
                 break;
             case 3:
-               int port2 = 50002; // Puerto del servidor
+               int port2 = 50001; // Puerto del servidor
 
                     try (ServerSocket serverSocket = new ServerSocket(port2)) {
                         System.out.println("Servidor esperando conexiones en el puerto " + port2 + "...");
@@ -160,13 +158,13 @@ public class matrices{
             default:
                 System.out.println("Opción no válida. Debe ser 1, 2 o 3.");
         }
-       System.out.println("IMPRIMIENDO MATRIZ C");
+       /*System.out.println("IMPRIMIENDO MATRIZ C");
        for(int i=0;i<matrizResultadoC.length;i++){
             for(int j=0;j<matrizResultadoC[0].length;j++){
                 System.out.print(matrizResultadoC[i][j]+" ");
             }
             System.out.println();
-       }
+       }*/
        // Inicializar una variable para almacenar la suma
         int suma = 0;
 
@@ -201,21 +199,21 @@ public class matrices{
                 for (int i = 0; i < submatricesA_grupo.length; i++) {
                     System.out.println("Grupo de submatriz " + i + ":");
                     int[][] submatriz = submatricesA_grupo[i];
-                    for (int j = 0; j < submatriz.length; j++) {
+                    /*for (int j = 0; j < submatriz.length; j++) {
                         for (int k = 0; k < submatriz[j].length; k++) {
                             System.out.print(submatriz[j][k] + " ");
                         }
                         System.out.println();
-                    }
+                    }*/
                 }
                 // Mostramos la matriz B transpuesta en el servidor
-                System.out.println("Matriz B transpuesta recibida del cliente:");
+                /*System.out.println("Matriz B transpuesta recibida del cliente:");
                 for (int i = 0; i < transposedMatrixB.length; i++) {
                     for (int j = 0; j < transposedMatrixB[i].length; j++) {
                         System.out.print(transposedMatrixB[i][j] + " ");
                     }
                     System.out.println();
-                }
+                }*/
                 // Crear la matriz resultado
                 int[][] resultado = new int[submatricesA_grupo.length][transposedMatrixB.length];
                 
@@ -233,7 +231,7 @@ public class matrices{
                                 
                                 //System.out.print((submatriz[j][l]*transposedMatrixB[k][l]) + " ");
                             }
-                            System.out.print(suma + " ");
+                            //System.out.print(suma + " ");
                             
                             resultado[i][k]=suma;
                         }
@@ -290,13 +288,13 @@ public class matrices{
             // Recibir la matriz resultado del servidor
             try{
                 int[][] resultMatrix = (int[][]) in.readObject();
-                System.out.println("\n\n\nMatriz resultado recibida desde el servidor para su construccion:");
+                /*System.out.println("\n\n\nMatriz resultado recibida desde el servidor para su construccion:");
                 for (int i = 0; i < resultMatrix.length; i++) {
                     for (int j = 0; j < resultMatrix[i].length; j++) {
                         System.out.print(resultMatrix[i][j] + " ");
                     }
                     System.out.println();
-                }
+                }*/
                 int numComputadora=(int)in.readObject();
                 System.out.println("NUMERO DE COMPUTADORA:" + numComputadora);
                 if(numComputadora==2){
