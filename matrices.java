14ddbf0e5ba819f650866a03cc206b9a4b9350e1
Victor <vrios718@gmail.com>
2023-10-09 09:08:02 -0600

    matrices terminado

--- matrices.java
+++ matrices.java
@@ -1,6 +1,5 @@
 import java.io.*;
 import java.net.*;
-import java.util.concurrent.CountDownLatch;
 public class matrices{
     // Crear una matriz de 6 renglones por 5 columnas (6x5)
             static int N = 6; // Número de renglones
